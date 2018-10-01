e4fb1501a27543355c2a02ad8a94b7c089a8ec3d
Steven Nemetz <snemetz@wiser.com>
2018-10-01 11:54:56 -0700

    Make more general

--- terraform-init.sh
+++ terraform-init.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 # TODO:
-#   move org to Variables
-#   move region to variable
-#   move state key to variable
+#   use default region ??
+org="wiser"
+region="us-west-2"
+service="cmp-service"
+
 profile=${1:-saml}
 [[ $(aws configure --profile ${profile} list 2>/dev/null) && $? -eq 0 ]] && \
   echo "Using AWS Profile: ${profile}" || \
   { echo "AWS Profile '${profile}' does not exist"; exit 1; }
 
-ENV=$(AWS_PROFILE=${profile} aws iam list-account-aliases --output=text --query 'AccountAliases[0]' | sed 's/wiser-//')
+ENV=$(AWS_PROFILE=${profile} aws iam list-account-aliases --output=text --query 'AccountAliases[0]' | sed "s/${org}-//")
 echo "AWS env: $ENV"
-BUCKET="wiser-$ENV-tf"
+BUCKET="${org}-$ENV-tf"
 echo "Using bucket=$BUCKET"
 rm -rf .terraform
 AWS_PROFILE=${profile} ENV=$ENV TF_VAR_env=$ENV terraform init -upgrade \
      -backend-config "bucket=$BUCKET" \
-     -backend-config "region=us-west-2" \
-     -backend-config "key=services/cmp-service.tfstate"
+     -backend-config "region=${region}" \
+     -backend-config "key=services/${service}.tfstate"
